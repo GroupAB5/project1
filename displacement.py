74703b995cb396e8590813f7f1128de288703e43
Adam <adaskoo112@gmail.com>
2020-03-19 11:25:52 +0100

    updated

--- displacement.py
+++ displacement.py
@@ -7,5 +7,16 @@ for file in os.listdir(dir):
     files.append(file)
 
 a = np.loadtxt(str(dir+files[0]), comments='%', usecols=(1, 2, 3))
-print(a)
 
+#returns a list with dates
+def get_dates(file_loc):
+    file = open(file_loc, 'r')
+
+    dates = []
+    line = file.readlines()
+    for i in range(2, len(line)):
+        dates.append(line[i].split()[0])
+
+    file.close()
+
+    return dates
