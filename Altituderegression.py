085fcd97d6c27b6144de6bce824643a8b1356ec7
Zakjezoutedrop <57629468+Zakjezoutedrop@users.noreply.github.com>
2020-03-24 18:20:48 +0100

    Linear fits of data on z
    this script will plot three regressions : total data, pre earthquake, post earthquake
    It is as of now a simple least squares regression but will soon do an update making weighted least squares solutions

---
+++ Altituderegression.py
@@ -0,0 +1,107 @@
+"""This Script plots data points in from  files converted by adam
+It will also plot three different linear regressions one red line over the complete data one line pink using
+data points from before the 2004 earthquake and one green using data points post earthquake
+To plot linear regression from a different file change the file name
+
+Note the x-axis numbers show the days counted from the first date in the file"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from sklearn.linear_model import LinearRegression
+import datetime as dt
+import matplotlib.dates as mdates
+import pandas as pd
+
+
+def abline(slope, intercept, x_vals, color):
+    """Plot a line from slope and intercept"""
+    axes = plt.gca()
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--',color =color)
+
+
+
+#file to be plotted needs to be in the same folder
+file = "ALGO"
+
+#pandas Dataframe
+Df = pd.read_csv(file , delim_whitespace=True, comment = '%',header=None)
+Df.columns = ["date", "x", "y", "z"]
+
+dates = Df["date"]
+y = pd.DataFrame(Df['z']).to_numpy()
+dateslist = [dt.datetime.strptime(d,'%y%b%d').date() for d in dates]
+
+daycount =[]
+for day in dateslist:
+    daycount.append((day-dateslist[0]).days)
+
+
+
+
+
+#Choosing the date to split the data On due to assumed discontinuity at earthquake
+Data_quake = dt.datetime(2004,12,26) in dateslist
+if Data_quake:
+    split = dateslist.index(dt.datetime(2004,12,26)) + 1                          #+1 as to include 26 in pre earthquake
+    print ('date of earth quake is included')
+else:
+    i = 0
+    lst =[]
+    for day in dates:
+        if day.startswith("04DEC27" or "04DEC28" or "04DEC29" or "04DEC30" or "04DEC31"):
+            lst.append([day,i])
+        i+=1
+    if  not lst:                                                                              #checking if list is empty
+        i = 0
+        lst = []
+        for day in dates:
+            if day.startswith("05" or "06" or "07" or "08" or "09" or "10" or "11" or "12" or "13" or "14" ):
+                lst.append([day,i])
+            i += 1
+    split = lst[0][1]
+    print("date of earthquake is not present data is split on :",lst[0][0])
+
+
+
+#linear regression over all dates with dates as input z valuies as output
+indexeddates = np.array(daycount).reshape(-1,1) #using indexes instead of actual dates.]
+model        = LinearRegression().fit(indexeddates,y)
+Rsq          = model.score(indexeddates,y)
+b0           = model.intercept_
+slope        = model.coef_
+
+# Linear regression assuming discontinuity
+datespre   = indexeddates[0:split]
+datespost  = indexeddates[split:-1]
+
+zpre       = y[0:split]
+zpost      = y[split:-1]
+
+modelpre   = LinearRegression().fit(datespre,zpre)
+modelpost  = LinearRegression().fit(datespost,zpost)
+
+b0pre      = modelpre.intercept_
+b0post     = modelpost.intercept_
+
+slopepre   = modelpre.coef_
+slopepost  = modelpost.coef_
+
+Rsqpre     = modelpre.score(datespre,zpre)
+Rsqpost    = modelpost.score(datespost,zpost)
+
+
+
+
+#plotting
+plt.scatter(indexeddates, y)
+abline(slope,b0,indexeddates,"red")
+abline(slopepre,b0pre,datespre,"pink")
+abline(slopepost,b0post,datespost,"green")
+
+plt.show()
+
+
+
+
