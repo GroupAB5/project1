231293075e7def48015a10af4a61293307992289
Cvh254 <62013665+Cvh254@users.noreply.github.com>
2020-06-11 10:17:23 +0200

    Add files via upload

---
+++ Final_ARIMA.py
@@ -0,0 +1,149 @@
+#Important Importing
+import warnings
+import itertools
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+from Altituderegression import timeseries
+
+#Loading Required Station
+file = r"KTPK.txt"
+n,e,u,Df = timeseries(file)
+
+#data
+y = Df
+
+#Test Data (Uncomment to use--also modify dates and column name)
+#data = sm.datasets.co2.load_pandas()
+#y = data.data
+
+#Resampling to monthly averages
+# The 'MS' string groups the data in buckets by start of the month
+y = y['N'].resample('MS').mean()
+print(y)
+# The term bfill means that we use the value before filling in missing values
+y = y.fillna(y.bfill())
+
+#Plot Data
+y.plot(figsize=(15, 6))
+plt.show()
+
+
+#Definition for Parameter Selection for ARIMA model
+def Par_sel(y):
+    # Define the p, d and q parameters to take any value between 0 and 2
+    p = d = q = range(0, 2)
+
+    # Generate all different combinations of p, q and q triplets
+    pdq = list(itertools.product(p, d, q))
+
+    # Generate all different combinations of seasonal p, q and q triplets
+    seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+    #Fitting best p,d,q to dataset
+    warnings.filterwarnings("ignore") # specify to ignore warning messages
+
+    for param in pdq:
+        for param_seasonal in seasonal_pdq:
+            try:
+                mod = sm.tsa.statespace.SARIMAX(y,
+                                                order=param,
+                                                seasonal_order=param_seasonal,
+                                                enforce_stationarity=False,
+                                                enforce_invertibility=False)
+
+                results = mod.fit()
+
+                print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+            except:
+                continue
+
+
+#Fitting an ARIMAX Time-series model
+mod = sm.tsa.statespace.SARIMAX(y,
+                                order=(1, 3, 0),
+                                seasonal_order=(0, 0, 0, 12),
+                                enforce_stationarity=False,
+                                enforce_invertibility=False)
+
+results = mod.fit()
+
+#Print summary of fitted model
+print(results.summary().tables[1])
+
+#Print diagnostics of fitted model
+results.plot_diagnostics(figsize=(15, 12))
+plt.show()
+            
+#Validating Forecasts
+pred = results.get_prediction(start=pd.to_datetime('2004-01-01'), dynamic=False)
+pred_ci = pred.conf_int()
+
+
+
+#### Producing and Testing non-dynamic and dynamic forecasts ####
+#Non-dynamic forecast
+ax = y['1999':].plot(label='observed')
+pred.predicted_mean.plot(ax=ax, label='One-step ahead Forecast', alpha=.7)
+
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.2)
+
+ax.set_xlabel('Date')
+ax.set_ylabel('Displacement')
+plt.legend()
+plt.show()
+#Quantifying accuracy of forecasts
+y_forecasted = pred.predicted_mean
+y_truth = y['2004-01-01':]
+# Compute the mean square error
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+#Dynamic forecasts
+pred_dynamic = results.get_prediction(start=pd.to_datetime('2004-01-01'), dynamic=True, full_results=True)
+pred_dynamic_ci = pred_dynamic.conf_int()
+#Plot dynamic forecasts
+ax = y['1999':].plot(label='observed', figsize=(20, 15))
+pred_dynamic.predicted_mean.plot(label='Dynamic Forecast', ax=ax)
+
+ax.fill_between(pred_dynamic_ci.index,
+                pred_dynamic_ci.iloc[:, 0],
+                pred_dynamic_ci.iloc[:, 1], color='k', alpha=.25)
+
+ax.fill_betweenx(ax.get_ylim(), pd.to_datetime('2004-01-01'), y.index[-1],
+                 alpha=.1, zorder=-1)
+ax.set_xlabel('Date')
+ax.set_ylabel('Displacement')
+plt.legend()
+plt.show()
+#Second accuracy Test
+# Extract the predicted and true values of our time series
+y_forecasted = pred_dynamic.predicted_mean
+y_truth = y['2004-01-01':]
+# Compute the mean square error
+mse = ((y_forecasted - y_truth) ** 2).mean()
+print('The Mean Squared Error of our forecasts is {}'.format(round(mse, 2)))
+
+
+#Producing the actual and final forecast
+# Get forecast 500 steps ahead in future
+pred_uc = results.get_forecast(steps=500)
+
+# Get confidence intervals of forecasts
+pred_ci = pred_uc.conf_int()
+
+#Plot forecast
+ax = y.plot(label='observed', figsize=(20, 15))
+pred_uc.predicted_mean.plot(ax=ax, label='Forecast')
+ax.fill_between(pred_ci.index,
+                pred_ci.iloc[:, 0],
+                pred_ci.iloc[:, 1], color='k', alpha=.25)
+ax.set_xlabel('Date')
+ax.set_ylabel('Displacement')
+
+plt.legend()
+plt.show()
